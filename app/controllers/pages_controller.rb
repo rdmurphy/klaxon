8664fdc7759c1498162680264fdae0cdd993776d
Ryan Murphy <rdmurphy@users.noreply.github.com>
2026-02-11 01:14:34 -0800

    Needs a counter column, but this fixes part of it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,7 +24,7 @@ class PagesController < ApplicationController
 
   # GET /pages
   def index
-    @pages = Page.order('created_at DESC').all
+    @pages = Page.includes(:user).order('created_at DESC')
   end
 
   # GET /pages/1
