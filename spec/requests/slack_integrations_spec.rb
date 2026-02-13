4db48b9cffe911f2538295d75a791dac07c66ad4
Ryan Murphy <rdmurphy@users.noreply.github.com>
2026-02-13 00:02:03 -0800

    Remove unused/empty/placeholder specs

--- spec/requests/slack_integrations_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "SlackIntegrations", type: :request do
-  describe "GET /slack_integrations" do
-  end
-end
--- spec/requests/sqs_integrations_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "SqsIntegrations", type: :request do
-  describe "GET /sqs_integrations" do
-  end
-end
