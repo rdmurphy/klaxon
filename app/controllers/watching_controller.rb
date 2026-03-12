ada4c6cd0e8cfe5ff71adc4674cc6e60a9a80210
Ryan Murphy <rdmurphy@users.noreply.github.com>
2026-03-11 23:38:52 -0700

    Build a list of changes and new snapshots

--- app/controllers/watching_controller.rb
+++ app/controllers/watching_controller.rb
@@ -2,6 +2,8 @@ class WatchingController < ApplicationController
   before_action :authorize
 
   def feed
-    @changes = Change.includes(after: { page: :user }).order("created_at DESC").first(20)
+    changes = Change.includes(after: { page: :user }).order(created_at: :desc).limit(20)
+    initial_snapshots = PageSnapshot.without_changes.includes(page: :user).order(created_at: :desc).limit(20)
+    @feed_items = (changes + initial_snapshots).sort_by(&:created_at).reverse.first(20)
   end
 end
